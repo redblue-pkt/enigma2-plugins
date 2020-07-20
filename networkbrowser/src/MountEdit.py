484093f14795f43dc621e0c97e569c59c3372942
persianpros <persianpros@yahoo.com>
2020-07-20 11:22:12 +0430

    Use SMB 3.0 like what we use in kernels

--- networkbrowser/src/MountEdit.py
+++ networkbrowser/src/MountEdit.py
@@ -165,7 +165,7 @@ class AutoMountEdit(Screen, ConfigListScreen):
 		if mounttype == "nfs":
 			defaultOptions = "rw,nolock,tcp"
 		else:
-			defaultOptions = "rw,utf8,vers=1.0"
+			defaultOptions = "rw,utf8,vers=3.0"
 		if self.mountinfo['sharename'] and self.mountinfo.has_key('sharename'):
 			sharename = re_sub("\W", "", self.mountinfo['sharename'])
 			self.old_sharename = sharename
@@ -253,7 +253,7 @@ class AutoMountEdit(Screen, ConfigListScreen):
 			if self.mounttypeConfigEntry.value == "nfs":
 				defaultOptions = "rw,nolock,tcp"
 			else:
-				defaultOptions = "rw,utf8,vers=1.0"
+				defaultOptions = "rw,utf8,vers=3.0"
 			if self.mountinfo.has_key('options'):
 				options = self.mountinfo['options']
 			else:
