86471dd7bbf87959f92e81e17f965d52ca27f40d
persianpros <persianpros@yahoo.com>
2021-02-27 22:39:38 +0330

    MC_Settings.py:  Get rid of getBoxType and getBoxBrand in messages, this is only needed in setup and startwizard

--- bmediacenter/src/MC_Settings.py
+++ bmediacenter/src/MC_Settings.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
 from Screens.Standby import TryQuitMainloop
-from enigma import getBoxType, getBoxBrand
 from Components.ActionMap import ActionMap, NumberActionMap
 from Components.Label import Label
 from Components.Sources.List import List
@@ -16,9 +15,6 @@ from Components.Console import Console
 from __init__ import _
 from os import mkdir
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-
-brand = getBoxBrand()
-model = getBoxType()
 
 mcpath = resolveFilename(SCOPE_PLUGINS, "Extensions/BMediaCenter/skins/defaultHD/images/")
 #try:
@@ -90,11 +86,11 @@ class MC_Settings(Screen):
 
 	def checkNetworkStateFinished(self, result, retval, extra_args=None):
 		if result.find('bad address') != -1:
-			self.session.openWithCallback(self.InstallPackageFailed, MessageBox, _("Your %s %s is not connected to the internet, please check your network settings and try again.") % (brand, model), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
+			self.session.openWithCallback(self.InstallPackageFailed, MessageBox, _("Your receiver is not connected to the internet, please check your network settings and try again."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
 		elif result.find('wget returned 1') != -1 or result.find('wget returned 255') != -1 or result.find('404 Not Found') != -1:
 			self.session.openWithCallback(self.InstallPackageFailed, MessageBox, _("Sorry feeds are down for maintenance, please try again later."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
 		else:
-			self.session.openWithCallback(self.InstallPackage, MessageBox, _('Your %s %s will be restarted after the installation of service\nReady to install %s ?') % (brand, model, self.service_name), MessageBox.TYPE_YESNO)
+			self.session.openWithCallback(self.InstallPackage, MessageBox, _('Your receiver will be restarted after the installation of service\nReady to install %s ?') % self.service_name, MessageBox.TYPE_YESNO)
 
 	def InstallPackage(self, val):
 		if val:
