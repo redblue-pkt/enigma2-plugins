6930202a12cc19099e899ae300d0cdf9dd432d80
persianpros <persianpros@yahoo.com>
2023-06-02 15:45:30 +0330

    [MerlinMusicPlayer] updates
    Co-authored-by: Mr.Servo <mrservo@gmx.de>
    Co-authored-by: Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
    Co-authored-by: jbleyel <jbleyel@gmx.net>

--- merlinmusicplayer/src/__init__.py
+++ merlinmusicplayer/src/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 import gettext
 
 PluginLanguageDomain = "MerlinMusicPlayer"
@@ -16,7 +15,7 @@ def _(txt):
 	if gettext.dgettext(PluginLanguageDomain, txt):
 		return gettext.dgettext(PluginLanguageDomain, txt)
 	else:
-		#print("[" + PluginLanguageDomain + "] fallback to default translation for " + txt)
+		#print "[" + PluginLanguageDomain + "] fallback to default translation for " + txt
 		return gettext.gettext(txt)
 
 
