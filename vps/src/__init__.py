74141ea84a7c356328e15aa0e3234bbed98f83f6
redblue-pkt <17146148+redblue-pkt@users.noreply.github.com>
2024-02-10 19:11:00 +0100

    add vps

---
+++ vps/src/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+import os
+import gettext
+
+PluginLanguageDomain = "vps"
+PluginLanguagePath = "SystemPlugins/vps/locale"
+
+
+def localeInit():
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+
+def _(txt):
+	if gettext.dgettext(PluginLanguageDomain, txt):
+		return gettext.dgettext(PluginLanguageDomain, txt)
+	else:
+		print("[" + PluginLanguageDomain + "] fallback to default translation for " + txt)
+		return gettext.gettext(txt)
+
+
+localeInit()
+language.addCallback(localeInit)
